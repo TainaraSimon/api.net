43c40e54da911cd191460bfee304a8feda341be9
TainaraSimon <tainarapsimon@gmail.com>
2025-04-21 21:58:39 -0300

    adicionado patch, put e post

--- api.csv/api.csv/Controllers/AnimaisController.cs
+++ api.csv/api.csv/Controllers/AnimaisController.cs
@@ -66,8 +66,64 @@ namespace api.csv.Controllers
            {
                 return BadRequest(e.Message); //400
            }
-            
-
         }
+
+        [HttpPatch("AlterarNome")]
+
+        public ActionResult<Animal> AlterarNome(int id, [Frombody] Animal body)
+        {
+            if ((body == null) ||
+                    (string.IsNullOrEmpty(body.Name)))
+                return BadRequest();
+
+            Animal animal =
+         _dbContext
+         .Animals.Find(a => a.Id == body.Id);
+
+            if (animal == null)
+                return NotFound();
+            animal.Name = body.Name;
+            return Ok(animal);
+        }
+
+        [HttpPut("AlterarTudo")]
+        public ActionResult<Animal> AlterarTudo(int id, [FromBody] Animal body)
+        {
+            Animal animal =
+                _dbContext
+                .Animals.Find(a => a.Id == id);
+
+            if (animal == null)
+                return NotFound(); //404
+
+            animal.Name = body.Name;
+            animal.Classification = body.Classification;
+            animal.Origin = body.Origin;
+            animal.Reproduction = body.Reproduction;
+            animal.Feeding = body.Feeding;
+
+            return Ok(animal);
+        }
+
+        [HttpPost("Inserir")]
+        public ActionResult<Animal> Inserir(int id, [FromBody] Animal body)
+        {
+            if (body == null)
+                return BadRequest("os dados para a inserção não foram preenchidos"); //400
+
+
+            Animal novoAnimal = new Animal();
+            novoAnimal.Name = body.Name;
+            novoAnimal.Id = _dbContext.Animals.Max(a => a.Id) + 1;
+            novoAnimal.Classification = body.Classification;
+            novoAnimal.Origin = body.Origin;
+            novoAnimal.Reproduction = body.Reproduction;
+            novoAnimal.Feeding = body.Feeding;
+
+            _dbContext.Animals.Add(novoAnimal);
+
+            return Ok(novoAnimal);
+        }
+
     }
 }
